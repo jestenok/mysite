0f0ae364ca879fcb7b70ba95a1f6493cd353ad36
jestenok <112510943+jestenok@users.noreply.github.com>
2023-03-23 03:23:07 +0800

    test jenkins webhook

--- mysite/settings.py
+++ mysite/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT'),
     }
 }
-
+# jenkins test
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
