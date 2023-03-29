bea136f29e338d78c9870bf77d44314725d69ea9
jestenok <112510943+jestenok@users.noreply.github.com>
2023-03-29 22:27:14 +0800

    moved collectctatic to entypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -6,6 +6,8 @@ python manage.py createcachetable
 
 python manage.py createsuperuser --noinput --username ${LOGIN} --email ${LOGIN}@${DOMAINNAME}
 
+python manage.py collectstatic --noinput
+
 gunicorn mysite.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
