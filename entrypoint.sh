dadb951a1edcfbc52b657e04df1d3d44e4973acf
jestenok <112510943+jestenok@users.noreply.github.com>
2023-03-31 01:06:23 +0800

    change image path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 python manage.py makemigrations
 python manage.py migrate
-
 python manage.py createcachetable
-
 python manage.py createsuperuser --noinput --username ${LOGIN} --email ${LOGIN}@${DOMAINNAME}
-
 python manage.py collectstatic --noinput
-
 gunicorn mysite.wsgi:application --bind 0.0.0.0:8080
-
 exec "$@"
